ee3672f0e151c6dd9a25a13f1c9f7c389707ea91
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-04-18 21:03:51 +0400

    add second player

--- tests/test_thing.py
+++ tests/test_thing.py
@@ -4,17 +4,27 @@ from app.thing import Thing
 
 
 class Game:
+    def __init__(self) -> None:
+        self.user_names = []
+
     def run(self, user_input: str):
-        return "players: Pippo"
+        self.user_names.append(user_input.split(" ")[-1])
+        return f"players: {', '.join(self.user_names)}"
+
 
 class TestThing(unittest.TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.game = Game()
 
-    def test_add_player(self):
-
+    def test_add_first_player(self):
         response = self.game.run("add player Pippo")
 
         self.assertEquals(response, "players: Pippo")
 
+    def test_add_second_player(self):
+        self.game.run("add player Pippo")
+
+        response = self.game.run("add player Pluto")
+
+        self.assertEquals(response, "players: Pippo, Pluto")
