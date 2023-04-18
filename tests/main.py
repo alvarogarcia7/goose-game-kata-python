033ffa3e3de67336a4c8cf98483c557d737c225c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-04-18 23:25:18 +0400

    move a player

--- tests/main.py
+++ tests/main.py
@@ -1,27 +1,59 @@
+from __future__ import annotations
+
+
+class Player:
+    def __init__(self, name: str, position: int) -> None:
+        self.name = name
+        self.position = position
+
+    def move(self, spaces):
+        self.position += spaces
+
+    def __eq__(self, o: object) -> bool:
+        return o.name == self.name and o.position == self.position
+
+
 class Game:
-    def __init__(self, players) -> None:
+    def __init__(self, players: PlayerRepository) -> None:
         self.players = players
         self.user_names = []
 
     def run(self, user_input: str):
-        user_name = user_input.split(" ")[-1]
+        user_input_parts = user_input.split(" ")
+        user_name = user_input_parts[-1]
         self.user_names.append(user_name)
+        action = user_input_parts[0]
+        if action == 'move':
+            user_name = user_input_parts[1]
+            die_values = list(map(lambda x: x.strip().replace(',', ''), user_input_parts[-2:]))
+            sum_of_die = sum(map(lambda x: int(x), die_values))
+            user = self.players.find_by_name(user_name)
+            previous_position = str(user.position)
+            if not user.position:
+                previous_position = 'Start'
+            user.move(sum_of_die)
+            current_position = user.position
+            return f'{user_name} rolls {", ".join(die_values)}. Pippo moves from {previous_position} to {current_position}'
         try:
-            self.players.add(user_name)
+            self.players.add(Player(user_name, 0))
         except Exception:
             return f"{user_name}: already existing player"
-        return f"players: {', '.join(self.players.find_all())}"
+        names = list(map(lambda player: player.name, self.players.find_all()))
+        return f"players: {', '.join(names)}"
 
 
 class PlayerRepository:
     def __init__(self):
         self.names = []
 
-    def add(self, name: str):
+    def add(self, name: Player):
         if name in self.names:
             raise Exception("Repeated user")
 
         self.names.append(name)
 
-    def find_all(self) -> [str]:
+    def find_all(self) -> [Player]:
         return self.names
+
+    def find_by_name(self, name):
+        return list(filter(lambda player: player.name == name, self.names))[0]
--- tests/test_thing.py
+++ tests/test_thing.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tests.main import Game, PlayerRepository
+from tests.main import Game, PlayerRepository, Player
 from unittest.mock import Mock, MagicMock
 
 
@@ -16,10 +16,10 @@ class TestGame(unittest.TestCase):
 
         self.game.run("add player Pippo")
 
-        self.players.add.assert_called_with('Pippo')
+        self.players.add.assert_called_with(Player('Pippo', 0))
 
     def test_formatting_of_the_resulting_players(self):
-        self.players.add('Pippo')
+        self.players.add(Player('Pippo', 0))
 
         response = self.game.run("add player Pluto")
 
@@ -32,6 +32,14 @@ class TestGame(unittest.TestCase):
 
         self.assertEquals(response, "Pippo: already existing player")
 
+    def test_moving_a_player(self):
+        self.players.add(Player('Pippo', 0))
+        self.players.add(Player('Pluto', 0))
+
+        response = self.game.run("move Pippo 4, 2")
+
+        self.assertEquals(response, "Pippo rolls 4, 2. Pippo moves from Start to 6")
+
 
 class TestPlayers(unittest.TestCase):
 
@@ -40,19 +48,28 @@ class TestPlayers(unittest.TestCase):
         self.players = PlayerRepository()
 
     def test_add_a_player(self):
-        self.players.add('player_name')
+        self.players.add(Player('player_name', 0))
 
-        self.assertEquals(self.players.find_all(), ['player_name'])
+        self.assertEquals(self.players.find_all(), [Player('player_name', 0)])
 
     def test_add_a_second_player(self):
-        self.players.add('player_name_1')
+        self.players.add(Player('player_name_1', 0))
 
-        self.players.add('player_name_2')
+        self.players.add(Player('player_name_2', 0))
 
-        self.assertEquals(self.players.find_all(), ['player_name_1', 'player_name_2'])
+        self.assertEquals(self.players.find_all(), [Player('player_name_1', 0), Player('player_name_2', 0)])
 
     def test_cannot_add_a_repeated_player(self):
-        self.players.add("Pippo")
+        self.players.add(Player("Pippo", 0))
 
         with self.assertRaises(Exception):
-            self.players.add("Pippo")
+            self.players.add(Player("Pippo", 0))
+
+
+class TestPlayer(unittest.TestCase):
+    def test_move_it(self):
+        player = Player('P1', 0)
+
+        player.move(1)
+
+        self.assertEquals(1, player.position)
